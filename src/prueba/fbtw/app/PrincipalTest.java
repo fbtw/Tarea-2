42f1b82baf4dd7e547e516bbe5b08b28ff7a3d52
fbtw <fborja@thoughtworks.com>
2017-06-21 15:58:23 -0500

    Agrega FileOutputS. para mostrar resultados en archivo

--- src/prueba/fbtw/app/PrincipalTest.java
+++ src/prueba/fbtw/app/PrincipalTest.java
@@ -4,6 +4,8 @@ import org.junit.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 import static java.lang.System.out;
 
@@ -12,12 +14,16 @@ public class PrincipalTest  {
     private final File file = new File("src/resource/usuario.txt");
     private String ingreso;
     private int ingreso2;
-    private final Biblioteca bangalore = new Biblioteca(Editorial.crearLibros(12));
+    private final Biblioteca bangalore = new Biblioteca(Editorial.crearLibros(10));
     private Scanner sc =null;
 
     @Before
     public void prep() throws FileNotFoundException {
         sc = new Scanner(file);
+        //Cambia el metodo out de System para que escriba en un archivo en vez de en consola.
+        PrintStream out = new PrintStream(new FileOutputStream("src/resource/ejemplo.txt"));
+        System.setOut(out);
+
     }
     @Test
     public void inicio() throws java.lang.Exception {
