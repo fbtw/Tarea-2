f41f061aec509125da60bf8abad2a09e378ef159
fbtw <fborja@thoughtworks.com>
2017-06-27 16:12:44 -0500

    Añade nueva clase Principal con nuevo hilo de ejecución

--- src/java/fbtw/app/Imprenta.java
+++ src/java/fbtw/app/Imprenta.java
@@ -4,40 +4,66 @@ import static java.lang.System.out;
 
 public class Imprenta {
 
-    static void imprimirInicio(String in){
+    static void imprimirInicio( ){
         out.print("    Bienvenido a"+"\n"+
                 "बैंगलोर सार्वजनिक पुस्तकालय"+"\n"+
                 "Biblioteca Bengalore"+"\n\n"+
                 "Pulse C para continuar"+"\n"+
-                "U:"+ in
+                "U:"
         );
     }
-    static void imprimirMenú(String in){
+    static void imprimirMenú( ){
         out.print("\n       MENÚ " +"\n"+
                 "Seleccione una opción:"+"\n"+
                 "* Buscar un artículo (B)"+"\n"+
-                // "* Devolver un artículo (D)"+"\n"+
+                "* Pedir un artículo (P)"+"\n"+
+                "* Devolver un artículo (D)"+"\n"+
                 "* Salir (S) "+"\n"+
-                "U:"+in
+                "U:"
         );
     }
-    static void imprimirMenú2(int in2){
-        out.print("\nSeleccione una opción" +
-                "\nU:"+in2);
+    static void imprimirMenúTipo( ){
+        out.print("\n       MENÚ " +"\n"+
+                "Seleccione el artículo:"+"\n"+
+                "* (L)ibro"+"\n"+
+                "* (R)evista"+"\n"+
+                "* (B)lu-Ray"+"\n"+
+                "* (T)ésis"+"\n"+
+                "* (E)nsayo"+"\n"+
+                "U:"
+        );
     }
-
-    static void imprimirDetalle(String in){
+    static void imprimirMenúPedir(){
+        out.print("\n       MENÚ " +"\n"+
+                "Seleccione el artículo que desea pedir\n"+
+                "U:"
+        );
+    }
+    static void imprimirMenúDevo(){
+        out.print("\n       MENÚ " +"\n"+
+                "Seleccione el artículo que desea devolver\n"+
+                "U:"
+        );
+    }
+    static void imprimirDetalle(){
         out.print("* Pedir el artículo (P)"+"\n"+
                 "* Devolver el artículo (D)"+"\n"+
                 "* Atras (A) "+"\n"+
-                "U:"+in
+                "U:"
         );
     }
     static void imprimirError(){
-        out.print(" \nOpción incorrecta, intente de nuevo"+"\n");
+        out.print("Opción incorrecta, intente de nuevo"+"\n");
     }
     static void imprimirErrorNúmero(){
-        out.print(" \nIngrese un número"+"\n");
+        out.print("Ingrese un número"+"\n");
     }
-    static void imprimirTamañoNúmero() { out.print(" \nEl número es muy grande"+"\n");}
+    static void imprimirTamañoNúmero() { out.print("El número es muy grande"+"\n");}
+
+    public static void imprimirVacio() {
+        out.print("Artículo no encontrado"+"\n");
+    }
+    public static void imprimirSalida() {
+        out.println("\n\n\tGracias por venir.");
+    }
 }
--- src/prueba/fbtw/app/PrincipalTest.java
+++ src/prueba/fbtw/app/PrincipalTest.java
@@ -1,35 +1,68 @@
 package fbtw.app;
 
+import javafx.scene.input.KeyCode;
 import org.junit.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.util.List;
 import java.util.Scanner;
+
+import static java.lang.System.in;
 import static java.lang.System.out;
 
 public class PrincipalTest  {
 
-    private final File file = new File("src/resource/usuario.txt");
-    private String ingreso;
-    private int ingreso2;
-    private final Biblioteca bangalore = new Biblioteca(Editorial.crearLibros(10));
-    private Scanner sc =null;
+    private static final File file = new File("src/resource/usuario.txt");
+    private static String ingreso;
+    private static int ingreso2;
+    private static Biblioteca bangalore = new Biblioteca();
+    private static Scanner sc =null;
+
+    @Test
+    public  void elegirTest ()throws java.lang.Exception {
+       // bangalore.setArtículos(Editorial.crearLibros(6),Tipo.Libro);
+        Imprenta.imprimirInicio();
+
+        String op="c";
+        String m=Principal.elegir("bienvenida",op);
+        Assert.assertEquals("principal", m);
+        op="b";
+        m=Principal.elegir(m,op);
+        Assert.assertEquals("tipo", m);
+        op="b";
+        m=Principal.elegir(m,op);
+        Assert.assertEquals("tipo", m);
+        op="l";
+        m=Principal.elegir(m,op);
+        Assert.assertEquals("artic", m);
+        op="1";
+        m=Principal.elegir(m,op);
+        Assert.assertEquals("detalle", m);
+        op="p";
+        m=Principal.elegir(m,op);
+        Assert.assertEquals("principal", m);
+        op="s";
+        m=Principal.elegir(m,op);
+        Assert.assertEquals("salir", m);
+    }
+
 
     @Before
     public void prep() throws FileNotFoundException {
         sc = new Scanner(file);
         // Cambia el metodo out de System para que escriba en un archivo en vez de en consola.
-        PrintStream out = new PrintStream(new FileOutputStream("src/resource/ejemplo4.txt"));
+        PrintStream out = new PrintStream(new FileOutputStream("src/resource/ejemplo.txt"));
         System.setOut(out);
 
     }
     @Test
     public void inicio() throws java.lang.Exception {
+        Imprenta.imprimirInicio();
         do {
             ingreso = sc.nextLine().toLowerCase();
-            Imprenta.imprimirInicio(ingreso);
             if (ingreso.equals("c")) {
                 menú();
                 break;
@@ -40,15 +73,16 @@ public class PrincipalTest  {
 
     @Test
     public void menú() throws java.lang.Exception {   //menú
+        Imprenta.imprimirMenú();
         do {
             ingreso = sc.nextLine().toLowerCase();
-            Imprenta.imprimirMenú(ingreso);
+
             if (ingreso.equals("b")) {
                 menú2();
                 break;
             }
             if (ingreso.equals("s")) {
-                salir();
+                Imprenta.imprimirSalida();
                 break;
             }
             Imprenta.imprimirError();
@@ -58,6 +92,10 @@ public class PrincipalTest  {
     @Test
     public void menú2() throws java.lang.Exception { //buscar
         ingreso2=0;
+
+        bangalore.detalleLibrosBiblioteca();
+       // Imprenta.imprimirMenú2();
+
         do {
             ingreso = sc.nextLine();
             try{
@@ -65,8 +103,6 @@ public class PrincipalTest  {
             } catch (NumberFormatException e) {
                 Imprenta.imprimirErrorNúmero();
             }
-            bangalore.detalleLibrosBiblioteca();
-            Imprenta.imprimirMenú2(ingreso2);
             if (ingreso2>bangalore.getLibros().size()){
                 Imprenta.imprimirTamañoNúmero();
             }
@@ -75,26 +111,25 @@ public class PrincipalTest  {
                 break;
             }
             else
-                Imprenta.imprimirError();
+                Imprenta.imprimirError();           //impo
         }while (true);
     }
     @Test
     public void detalle() throws java.lang.Exception {
+        if (ingreso2==0)ingreso2=1;
+        Artículo libro=bangalore.getLibros().get(ingreso2-1);
+        libro.detalle();
+        Imprenta.imprimirDetalle();
         do {
             ingreso = sc.nextLine().toLowerCase();
-            if (ingreso2==0)ingreso2=1;    //Solo para probar detalle() de forma individual
-            Libro libroDetallado = bangalore.getLibros().get(ingreso2-1);
-            libroDetallado.detalleLibro();
-
-            Imprenta.imprimirDetalle(ingreso);
 
             if (ingreso.equals("p")) {
-                libroDetallado.pedir();
+                libro.pedir();
                 menú();
                 break;
             }
             if (ingreso.equals("d")) {
-                libroDetallado.devolver();
+                libro.devolver();
                 menú();
                 break;
             }
@@ -106,9 +141,5 @@ public class PrincipalTest  {
         }while (true);
     }
 
-    @Test
-    public void salir() {
-        out.println("\n\n\tGracias por venir.");
-    }
 
 }
